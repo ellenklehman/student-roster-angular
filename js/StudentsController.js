5060430ea74f583df703404e827ed8e7eaa22519
Randy Leighton & Ellen Krause Lehman <student@epicodus.com>
2014-09-17 09:51:43 -0700

    initial commit student list showing

---
+++ js/StudentsController.js
@@ -0,0 +1,9 @@
+studentRoster.controller('StudentsCtrl', function StudentsCtrl($scope) {
+  $scope.students = [
+    { "name": "Chuck Norris" },
+    { "name": "Mr. Rogers" },
+    { "name": "The Pope" },
+    { "name": "Willy Wonka" },
+    { "name": "Gerry Gerbal" }
+  ]
+});
